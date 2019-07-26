04f356d7c3fb3d5921576217953115fb986dc457
Jeff Wilkey <jeffrotull92@gmail.com>
2019-07-25 21:59:16 -0700

    initial server setup

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan')
+const errorhandler = require('errorhandler');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+
+if (process.env.NODE_ENV === 'development') {
+  // only use in development
+  app.use(errorhandler())
+}
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+})
+
+module.exports = app;
